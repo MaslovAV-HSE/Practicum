f7387257456b40b5a2ffcf9c08a6c60f86bf5f4c
Alexandr Maslov <maslovsanav@gmail.com>
2024-03-30 00:35:30 +0500

    fixing button in profile page minor changes

--- src/pages/edit-password-page/index.js
+++ src/pages/edit-password-page/index.js
@@ -1,2 +1,2 @@
-import './profile-page.scss';
+import './edit-password-page.scss';
 export { default as EditPassword } from './edit-password-page.hbs?raw';
--- src/pages/edit-profile-page/index.js
+++ src/pages/edit-profile-page/index.js
@@ -1,2 +1,2 @@
-import './profile-page.scss';
+import './edit-profile-page.scss';
 export { default as EditProfile } from './edit-profile-page.hbs?raw';
